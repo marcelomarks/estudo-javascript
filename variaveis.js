5452999ec8b0d7d6de6395edcb2694242a93da7d
marcelomarks <marcelomarkscarmo@gmail.com>
2019-02-06 22:24:49 -0200

    aprendendo a concatenar variavel no javascript

--- variaveis.js
+++ variaveis.js
@@ -1,3 +1,18 @@
 console.log(pi);
 var pi = 3.141592;
 console.log(pi);
+
+var nome = "Marcelo";
+var sobreNome = "Marks";
+var ultimoNome = "Carmo";
+var idade = 27;
+var peso = 85;
+var sexo = "Mascolino";
+
+console.log("Nome: " + nome);
+console.log("Sobre Nome: " + sobreNome);
+console.log("Ultimo Nome: " + ultimoNome);
+console.log("Idade: " + idade);
+console.log("Peso: " + peso);
+console.log("Sexo: " + sexo);
+
